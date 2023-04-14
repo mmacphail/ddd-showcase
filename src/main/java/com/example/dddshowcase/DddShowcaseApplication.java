921ac59ce79cb2b76281bb303488c981af9a1c69
mmacphail <alexandre.fruchaud@gmail.com>
2023-04-14 06:48:29 +0200

    first commit

---
+++ src/main/java/com/example/dddshowcase/DddShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddShowcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dddshowcase/DddShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddshowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddShowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
