044f6dd85e355f7cc1d8185d65b5644ec1f2b39e
mmacphail <alexandre.fruchaud@gmail.com>
2023-05-17 11:47:20 +0200

    With observability dashboards

--- src/main/java/com/example/dddshowcase/resources/infrastructure/ResourceReferentialController.java
+++ src/main/java/com/example/dddshowcase/resources/infrastructure/ResourceReferentialController.java
@@ -6,12 +6,16 @@ import com.example.dddshowcase.resources.application.OnboardResourceCommand;
 import com.example.dddshowcase.resources.application.UploadResourceReferentialApi;
 import com.example.dddshowcase.resources.domain.model.ResourceReferential;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Random;
 import java.util.UUID;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/resourceReferential")
 public class ResourceReferentialController {
@@ -36,8 +40,18 @@ public class ResourceReferentialController {
         onboardResourceApi.onboardResource(OnboardResourceCommand.builder().resourceReferentialId(id).build());
     }
 
+    private final Random rng = new Random();
+
     @GetMapping
-    List<ResourceReferential> findAllResourceReferentials() {
-        return findAllResourceReferentialsApi.findAll();
+    ResponseEntity<List<ResourceReferential>> findAllResourceReferentials() throws InterruptedException {
+        if(rng.nextBoolean()) {
+            log.info("Working");
+            Thread.sleep(500);
+            return ResponseEntity.ok(findAllResourceReferentialsApi.findAll());
+        } else {
+            log.error("Internal server error");
+            Thread.sleep(2000);
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
